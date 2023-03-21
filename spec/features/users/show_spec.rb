60aefde8603c2789cec5b7edd6e5b06e4204b290
Weston Sandfort <sandfortw@gmail.com>
2023-03-21 15:51:56 -0600

    Add initial tests for dashboard page
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'user dashboard (show page)' do
+  before do
+    @user = User.create!(name: 'Homer', email: 'Homer@springfield.com')
+    visit user_path(@user)
+  end
+  
+  it 'has a header' do
+    expect(page).to have_content("#{@user.name}'s Dashboard")
+  end
+
+  it 'has a button to discover movies' do
+    expect(page).to have_button "Discover Movies"
+  end
+
+  it 'has a section that lists viewing parties' do
+    expect(page).to have_content("My Viewing Parties")
+  end
+end
