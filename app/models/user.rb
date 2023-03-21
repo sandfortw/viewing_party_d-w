6ecc9c72b7a0236cc4d6a23b8392407fd0357500
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-21 15:11:23 -0600

    feat: Add validation and uniqueness checks for creation of user
    Co-authored-by: Weston Sandfort <sandfortw@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@
 class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+
 end
