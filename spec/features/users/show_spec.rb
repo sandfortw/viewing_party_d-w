2912df9bed604b04c1406966ff5c2410ebcdf530
Weston Sandfort <sandfortw@gmail.com>
2023-03-21 16:02:49 -0600

    add tests for discover movies button

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -10,11 +10,18 @@ describe 'user dashboard (show page)' do
     expect(page).to have_content("#{@user.name}'s Dashboard")
   end
 
-  it 'has a button to discover movies' do
-    expect(page).to have_button "Discover Movies"
-  end
-
   it 'has a section that lists viewing parties' do
     expect(page).to have_content("My Viewing Parties")
+  end
+
+  describe 'discover page button' do
+    it 'has a button to discover movies' do
+      expect(page).to have_button "Discover Movies"
+    end
+
+    it 'links to a user\'s discover page' do
+      click_button 'Discover Movies' 
+      expect(current_path).to eq(user_discover_path(@user))
+    end
   end
 end
