a1b137369a816287678b1c5678096a9f1e098723
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-21 15:10:28 -0600

    feat: Add method for creating new user
    Co-authored-by: Weston Sandfort <sandfortw@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
   def new; end
 
   def show; end
+
+  def create
+    user = User.new(user_params)
+    unless user.save
+      flash[:error] = user.errors.full_messages.join(", ")
+      redirect_to register_path
+    else
+      flash[:notice] = "User: #{user.name}, has been created!"
+      redirect_to user_path(user)
+    end
+  end
+
+  private
+    def user_params
+      params.permit(:name, :email)
+    end
 end
