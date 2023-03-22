26802e9c91aef538e42de9ed13b06e8b56c5ff6d
Weston Sandfort <sandfortw@gmail.com>
2023-03-21 20:33:07 -0600

    top 20 movies passes basic test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,17 @@
 class MoviesController < ApplicationController
 
   def index
-    require 'pry'; binding.pry
+    #TOP 20
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['Authorization'] = ENV['movie_db_token']
+    end
+    response = conn.get('/3/movie/top_rated?&page=1')
+    data = JSON.parse(response.body, symbolize_names: true)
+    @movies = data[:results]
   end
 
   def show
 
   end
+
 end
