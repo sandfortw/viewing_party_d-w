96ccd6236ab5d13e42373c34ef23fb48b56df7ef
Weston Sandfort <sandfortw@gmail.com>
2023-03-22 11:32:14 -0600

    Update movie controller index to include search
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,10 +5,19 @@ class MoviesController < ApplicationController
     conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
       faraday.headers['Authorization'] = ENV['movie_db_token']
     end
-    response = conn.get('/3/movie/top_rated?&page=1')
-    data = JSON.parse(response.body, symbolize_names: true)
-    @movies = data[:results]
     @user = User.find(params[:user_id])
+    if params[:search].nil?
+      response = conn.get('/3/movie/top_rated?&page=1')
+      data = JSON.parse(response.body, symbolize_names: true)
+      @movies = data[:results]
+    elsif params[:search].blank?
+      flash[:notice] = "Field cannot be blank"
+      redirect_to user_discover_path(@user)
+    else
+      response = conn.get("/3/search/movie?&query=#{params[:search]}&page=1&include_adult=false")
+      data = JSON.parse(response.body, symbolize_names: true)
+      @movies = data[:results]
+    end
   end
 
   def show
