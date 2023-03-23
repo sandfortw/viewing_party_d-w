eb1dd815fbc0a22d9381163ef6a27ab68f47648b
Weston Sandfort <sandfortw@gmail.com>
2023-03-23 16:53:10 -0600

    Create test skeletons for viewing parties userpage
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -26,4 +26,80 @@ describe 'user dashboard (show page)' do
       expect(current_path).to eq(user_discover_path(@user))
     end
   end
+
+  describe 'viewing parties section (invited)' do
+    before do
+      @user_2 = User.create!(name: 'Lenny', email: 'Drunk@springfield.com')
+      party = Party.create!(movie_id: 808, host_id: @user_2.id, date: '2023-01-01', time: '12:00', duration: 180)
+      UserParty.create!(user_id: @user.id, party_id: party.id)
+      UserParty.create!(user_id: @user_2.id, party_id: party.id)
+      visit user_path(@user)
+    end
+
+    it 'should have the image for the movie' do
+
+    end
+
+    it 'should have the title for the party, which links to its show page' do
+
+    end
+
+    it 'should have the date and time for the party' do
+      
+    end
+
+    it 'should have who is hosting the event' do
+
+    end
+
+    it 'should have of users invited to the party, with user name in bold' do
+
+    end
+  end
+
+  describe 'viewing parties section (hosting)' do
+    before do
+      @user_2 = User.create!(name: 'Lenny', email: 'Drunk@springfield.com')
+      party = Party.create!(movie_id: 808, host_id: @user.id, date: '2023-01-01', time: '12:00', duration: 180)
+      UserParty.create!(user_id: @user.id, party_id: party.id)
+      UserParty.create!(user_id: @user_2.id, party_id: party.id)
+      visit user_path(@user)
+    end
+
+    it 'says that I am host of the party' do
+
+    end
+  end
+
+  describe 'viewing parties (multiple)' do
+    before do
+      @lenny = User.create!(name: 'Lenny', email: 'Drunk@springfield.com')
+      @bart = User.create!(name: 'Bart', email: 'Bart@springfield.com')
+      @lisa = User.create!(name: 'Lisa', email: 'Lisa@springfield.com')
+      shrek_party = Party.create!(movie_id: 808, host_id: @user.id, date: '2023-01-01', time: '12:00', duration: 180)
+      godfather_party = Party.create!(movie_id: 238, host_id: @user_2.id, date: '2023-05-01', time: '13:00', duration: 180)
+      UserParty.create!(user_id: @user.id, party_id: party.id)
+      UserParty.create!(user_id: @user_2.id, party_id: party.id)
+      UserParty.create!(user_id: @user_3.id, party_id: party.id)
+      UserParty.create!(user_id: @user_4.id, party_id: party.id)
+
+      UserParty.create!(user_id: @user.id, party_id: party2.id)
+      UserParty.create!(user_id: @user_2.id, party_id: party2.id)
+      UserParty.create!(user_id: @user_3.id, party_id: party2.id)
+      visit user_path(@user)
+    end
+
+    it 'displays multiple parties' do
+
+    end
+
+    it 'has all the people invited to the parties' do
+      
+    end
+
+    it 'says whether one is hosting or invited' do
+
+    end
+  end
+
 end
