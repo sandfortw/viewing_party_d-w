2b2193d9806387eff0b125a7cfeed0107cdaaa9d
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-23 14:17:55 -0600

    feature: Add new method to parties controller and view
    Co-authored-by: Weston Sandfort <sandfortw@gmail.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
 class PartiesController < ApplicationController
-  def new; end
+  def new 
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['Authorization'] = ENV['movie_db_token']
+    end
+
+    response = conn.get("3/movie/#{params[:movie_id]}")
+    data = JSON.parse(response.body, symbolize_names: true)
+    @movie = data
+    
+    @user = User.find(params[:user_id])
+    @users = User.where.not(id: @user.id)
+  end
+
+  # def show
+  #   conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+  #     faraday.headers['Authorization'] = ENV['movie_db_token']
+  #   end
+  #   @user = User.find(params[:user_id])
+  #   response = conn.get("3/movie/#{params[:id]}")
+  #   data = JSON.parse(response.body, symbolize_names: true)
+  #   @movie = data
+  #   response = conn.get("3/movie/#{params[:id]}/credits?&page=1")
+  #   data = JSON.parse(response.body, symbolize_names: true)
+  #   @cast = data[:cast]
+  #   response = conn.get("3/movie/#{params[:id]}/reviews")
+  #   data = JSON.parse(response.body, symbolize_names: true)
+  #   @reviews = data[:results]
+  # end
 end
