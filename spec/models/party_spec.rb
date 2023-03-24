119e340e6b694c81548e283e1ceac372536e25de
Weston Sandfort <sandfortw@gmail.com>
2023-03-24 09:56:44 -0600

    create model tests for party
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -15,4 +15,47 @@ RSpec.describe Party, type: :model do
     it { should validate_presence_of(:host_id) }
     it { should validate_presence_of(:movie_id) }
   end
+
+  before do
+    @user_1 = User.create!(name: 'Dawson', email: 'DawsonTimmons@gmail.com')
+    @user_2 = User.create!(name: 'Weston', email: 'Sandfortw@gmail.com')
+    @user_3 = User.create!(name: 'Homer', email: 'Homer@springfield.com')
+    @user_4 = User.create!(name: 'Marge', email: 'Marge@springfield.com')
+    @user_5 = User.create!(name: 'Maggie', email: 'Maggie@springfield.com')
+    @user_6 = User.create!(name: 'Bart', email: 'Bart@springfield.com')
+    @user_7 = User.create!(name: 'Lisa', email: 'Lisa@springfield.com')
+    @user_8 = User.create!(name: 'Krusty', email: 'Krusty@springfield.com')
+    @user_9 = User.create!(name: 'Mr.Burns', email: 'CEO@springfield.com')
+    @user_10 = User.create!(name: 'Lenny', email: 'Drunk@springfield.com')
+
+    @party_1 = Party.create!(movie_id: 808, host_id: @user_1.id, date: '2023-01-01', time: '12:00', duration: 300)
+    @party_2 = Party.create!(movie_id: 238, host_id: @user_2.id, date: '2023-02-01', time: '06:00', duration: 300)
+
+    @user_party_1 = UserParty.create!(user_id: @user_1.id, party_id: @party_1.id)
+    @user_party_2 = UserParty.create!(user_id: @user_3.id, party_id: @party_1.id)
+    @user_party_3 = UserParty.create!(user_id: @user_4.id, party_id: @party_1.id)
+    @user_party_4 = UserParty.create!(user_id: @user_5.id, party_id: @party_1.id)
+    @user_party_5 = UserParty.create!(user_id: @user_6.id, party_id: @party_1.id)
+
+    @user_party_6 = UserParty.create!(user_id: @user_2.id, party_id: @party_2.id)
+    @user_party_7 = UserParty.create!(user_id: @user_7.id, party_id: @party_2.id)
+    @user_party_8 = UserParty.create!(user_id: @user_8.id, party_id: @party_2.id)
+    @user_party_9 = UserParty.create!(user_id: @user_9.id, party_id: @party_2.id)
+    @user_party_10 = UserParty.create!(user_id: @user_10.id, party_id: @party_2.id)
+  end
+
+  describe '#movie', :vcr do
+    it 'should return a movie' do
+      expect(@party_1.movie[:original_title]).to eq 'Shrek'
+      expect(@party_2.movie[:original_title]).to eq 'The Godfather'
+    end
+  end
+
+  describe '#host_name' do
+    it 'should return the host name' do
+      expect(@party_1.host_name).to eq 'Dawson'
+      expect(@party_2.host_name).to eq 'Weston'
+    end
+  end
+
 end
