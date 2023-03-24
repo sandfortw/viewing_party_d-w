5233f99fe0d30f3e0b2aa88ce60552d9a7bbc145
Weston Sandfort <sandfortw@gmail.com>
2023-03-23 19:40:51 -0600

    complete prototype page for user dashboard
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -9,4 +9,20 @@ class Party < ApplicationRecord
   validates :host_id, presence: true
   validates :movie_id, presence: true
   validates :duration, presence: true
+
+
+  def movie
+    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+      faraday.headers['Authorization'] = ENV['movie_db_token']
+    end
+      response = conn.get("3/movie/#{self.movie_id}")
+      JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def host_name
+    User.find(self.host_id).name
+  end
 end
+
+
+
