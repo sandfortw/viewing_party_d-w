c6d716c1c803bf4acecb3c8d52f60247b99f6585
Weston Sandfort <sandfortw@gmail.com>
2023-03-24 14:58:59 -0600

    format with rubocop
    Co-authored-by: Dawson Timmons <dawsontimmons@gmail.com>

--- spec/services/api_service_spec.rb
+++ spec/services/api_service_spec.rb
@@ -6,7 +6,7 @@ describe ApiService do
   describe 'class methods' do
     describe '.movie_call(url)' do
       it 'returns a hash object', :vcr do
-        call = ApiService.movie_call("3/movie/808")
+        call = ApiService.movie_call('3/movie/808')
         expect(call).to be_a(Hash)
         expect(call[:id]).to eq(808)
         expect(call[:original_title]).to eq('Shrek')
