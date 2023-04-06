c91986ba3a870b583ba15a6afe0dd1ae2caf1219
Dawson Timmons <DawsonTimmons@gmail.com>
2023-04-05 18:16:00 -0600

    refactor routes to no longer include the id in the url

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   get '/login', to: 'users#login_form'
   post '/login', to: 'users#login_user'
+  get '/logout', to: 'users#logout_user'
+  get '/dashboard', to: 'users#show'
+  get '/discover', to: 'discover#index'
+  get '/movies', to: 'movies#index'
   resources :users, only: %i[create]
-  resources :users, only: %i[show] do
-    get '/discover', to: 'discover#index'
-    resources :movies, only: %i[index]
-    resources :movies, only: %i[show] do
-      get '/viewing-party/new', to: 'parties#new' # refactor?
-      resources :parties, only: %i[create]
-    end
+  resources :movies, only: %i[show] do
+    get '/viewing-party/new', to: 'parties#new' # refactor?
+    resources :parties, only: %i[create]
   end
 end
 
